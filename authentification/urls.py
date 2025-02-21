7cf1d4279464b84c479dc1f19392e72694d94901
Mamy <mamitianarandrianantenaina253@gmail.com>
2025-02-22 01:30:07 +0300

    corrections

--- authentification/urls.py
+++ authentification/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path
-from .views import login_view , register , logout_view , profile
-
+from . import views
 urlpatterns = [
-    path('register/',register,name='register'),
-    path('login/',login_view,name='login'),
-    path('logout/',logout_view,name='logout'),
-    path('profile/',profile, name='profile')
+   
 ]
