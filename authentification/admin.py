f7ad8f06ead7cb9a59ab1dd2d2a751d5c40107dc
Mamy <mamitianarandrianantenaina253@gmail.com>
2025-04-29 20:53:44 +0300

    create models for Customer and registre admin

--- authentification/admin.py
+++ authentification/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Customer
 
 # Register your models here.
+# Customer admin
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'email', 'city', 'sex', 'role', 'created_at')
+    search_fields = ('user__username', 'user__email', 'city')
+    def email(self, obj):
+        return obj.user.email
+
+admin.site.register(Customer, CustomerAdmin)
--- authentification/models.py
+++ authentification/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
 
 # Create your models here.
 class Customer(models.Model):
@@ -8,12 +10,29 @@ class Customer(models.Model):
         ('F', 'Feminin'),
     )
 
+    ROLE_TYPES = (
+        ('admin', 'Administrateur'),
+        ('client', 'Client'),
+    )
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
     sex = models.CharField(max_length=1, choices=SEX_TYPES)
     city = models.CharField(max_length=100)
+    role = models.CharField(max_length=10, choices=ROLE_TYPES, default='client')
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def clean(self):
+        if self.role == 'admin':
+            admin_count = Customer.objects.filter(role='admin')
+            if self.pk is None and admin_count.count() >= 10:
+                raise ValidationError("Le nombre maximale d'Admin est 10")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()  
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return self.user.username
+        return f"{self.user.username} ({self.get_role_display()})"
+
